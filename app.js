ce4a6b6dbaf1722e78331b4d440c0cb5e73cb5b4
gudetama <daniellewwise@gmail.com>
2020-11-11 20:33:31 -0500

    fixed prompt and writefile issues

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ const Intern = require("./scripts/intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
+
 const render = require("./scripts/htmlRenderer");
-const { run } = require("jest");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
@@ -111,36 +112,17 @@ function runQuestions() {
       runQuestions();
       }
       else {
-        return employees;
-        
+          const html = render(employees);
+          return fs.writeFile(outputPath, html, function (err) {
+            if (err) {
+           console.log("ERROR: failed to create html file");
+         } else {
+           console.log("New webpage generated");
+         }
+       });
       }
     });
 }
 
-const generateHTML = (employees) => {
-  return render(employees);
-}
 
-runQuestions().then((employees) => {
-    const html = generateHTML(employees);
-
-    return fs.readFile(outputPath, html);
-  }).then(() => {console.log("html file created");}).catch((err) => console.log(err));
-
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-
-
-
-
-
-
+runQuestions();
